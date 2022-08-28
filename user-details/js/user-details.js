502608553b009537bc35375841c5f75894ba1080
Martozhyna <mybonanzaluck@gmail.com>
2022-08-29 02:01:01 +0300

    add recursion

--- user-details/js/user-details.js
+++ user-details/js/user-details.js
@@ -1,7 +1,5 @@
-
 let url = new URL(location.href);
 let id = url.searchParams.get('id');
-
 
 let head = document.createElement('div');
 head.classList.add('head');
@@ -24,37 +22,56 @@ let divTitle = document.createElement('div');
 divTitle.classList.add('title');
 document.body.appendChild(divTitle)
 
- fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
     .then((response) => {
-        return response.json();
-    })
-    .then(users => {
-        for (const usersKey in users) {
-            if ( typeof users[usersKey] !== 'object') {
+               return response.json();
+           })
+    .then( users =>
+    {function explorer (user) {
+        for (const userKey in user) {
+            if (typeof user[userKey] !== 'object') {
                 let divInf = document.createElement('div');
-                divInf.innerHTML = `<b>${usersKey}</b>:${users[usersKey]}`;
+                divInf.innerHTML = `<b>${userKey}</b>:${user[userKey]}`;
                 div.appendChild(divInf);
             } else {
-                let array = users[usersKey];
-                for (const arrayKey in array) {
-                    if (typeof array[arrayKey] !== 'object') {
-                        let divInfor = document.createElement('div');
-                        divInfor.innerHTML = `<b>${arrayKey}</b>:${array[arrayKey]}`;
-                        div.appendChild(divInfor);
-                    } else {
-                        let miniArray = array[arrayKey];
-                        for (const miniArrayKey in miniArray) {
-                            if (typeof miniArray[miniArrayKey] !== 'object') {
-                                let divInformation = document.createElement('div');
-                                divInformation.innerHTML = `<b>${miniArrayKey}</b>: ${miniArray[miniArrayKey]}`;
-                                div.appendChild(divInformation);
-                            }
-                        }
-                    }
-                }
+                explorer(user[userKey]);
             }
         }
-    });
+
+    }explorer(users);
+    })
+
+ // fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+ //    .then((response) => {
+ //        return response.json();
+ //    })
+ //    .then(users => {
+ //        for (const usersKey in users) {
+ //            if ( typeof users[usersKey] !== 'object') {
+ //                let divInf = document.createElement('div');
+ //                divInf.innerHTML = `<b>${usersKey}</b>:${users[usersKey]}`;
+ //                div.appendChild(divInf);
+ //            } else {
+ //                let array = users[usersKey];
+ //                for (const arrayKey in array) {
+ //                    if (typeof array[arrayKey] !== 'object') {
+ //                        let divInfor = document.createElement('div');
+ //                        divInfor.innerHTML = `<b>${arrayKey}</b>:${array[arrayKey]}`;
+ //                        div.appendChild(divInfor);
+ //                    } else {
+ //                        let miniArray = array[arrayKey];
+ //                        for (const miniArrayKey in miniArray) {
+ //                            if (typeof miniArray[miniArrayKey] !== 'object') {
+ //                                let divInformation = document.createElement('div');
+ //                                divInformation.innerHTML = `<b>${miniArrayKey}</b>: ${miniArray[miniArrayKey]}`;
+ //                                div.appendChild(divInformation);
+ //                            }
+ //                        }
+ //                    }
+ //                }
+ //            }
+ //        }
+ //    }); //loops
 
 btn.onclick = function () {
     fetch(`https://jsonplaceholder.typicode.com/users/${id}/posts`)
